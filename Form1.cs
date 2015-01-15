36784864f8c92ed3d275cad7380f92c835dbcc63
SweetGirlsVp <sweetgirlsvp@gmail.com>
2015-01-15 19:36:49 -0200

    test
    asd

---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace PicasaDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        List<string> linksToAccess;
+        List<string> linksToImages;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string link = Misc.ParseUrl(textBox1.Text);
+            link = Regex.Replace(link, @"\?&firstFileToShow=([0-9]+)", "");
+            string linkNext = link + "?&firstFileToShow=";
+
+
+            Match mId = Regex.Match(link, @"all-images/([0-9a-zA-Z]+)/");
+            string id = mId.Groups[1].Value.ToString();
+
+
+            label2.Text = "Started download";
+            foreach (Control c in this.Controls)
+                c.Enabled = false;
+
+            Thread tr = new Thread(() =>
+            {
+                string content = Misc.GetHtml(link);
+                MatchCollection mcLinks = Regex.Matches(content, @"javascript:viewAllNavigate\(([0-9]+)\);");
+
+                linksToAccess = new List<string>();
+                linksToAccess.Add(link);
+                foreach (Match m in mcLinks)
+                    if (!linksToAccess.Contains(linkNext + m.Groups[1].Value.ToString()))
+                        linksToAccess.Add(linkNext + m.Groups[1].Value.ToString());
+
+                Invoke(new MethodInvoker(delegate() { label2.Text = "Getting all images from all pages"; }));
+                linksToImages = new List<string>();
+                foreach (string l in linksToAccess)
+                {
+                    string cLink = Misc.GetHtml(l);
+                    MatchCollection mcPhotos = Regex.Matches(cLink, @"<img src=""(.*)""/>");
+
+                    foreach (Match m in mcPhotos)
+                        linksToImages.Add(m.Groups[1].Value.ToString());
+                }
+
+                Invoke(new MethodInvoker(delegate() { label2.Text = "Downloading " + linksToImages.Count.ToString() + " images from all pages"; }));
+                string path = System.IO.Directory.GetCurrentDirectory().ToString() + @"\images\" + id.ToString() + @"\";
+
+                if(!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+
+                foreach (string i in linksToImages)
+                {
+                    Invoke(new MethodInvoker(delegate() { label2.Text = "Downloading #" + linksToImages.IndexOf(i).ToString() + " from " + linksToImages.Count.ToString(); }));
+                    Image im = Misc.GetImageFromUrl(i, path + linksToImages.IndexOf(i).ToString() + ".jpg");
+
+                    if (im == null)
+                        Invoke(new MethodInvoker(delegate() { richTextBox1.Text += "Error on #" + linksToImages.IndexOf(i.ToString()) + Environment.NewLine;   }));
+                        
+                }
+
+
+                Invoke(new MethodInvoker(delegate() {
+                    foreach (Control c in this.Controls)
+                        c.Enabled = true;
+
+                    label2.Text = "End!";
+                }));
+            });
+            tr.IsBackground = true;
+            tr.Start();
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Image m = Misc.GetImageFromUrl(@"http://dc481.4shared.com/img/c55uplZ6/s7/14098f70bf0/DSC_0062", @"C:\a.jpg");
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
---
+++ Misc.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+using System.Security.Cryptography;
+using System.Drawing;
+
+namespace PicasaDownloader
+{
+    public static class Misc
+    {
+        public static bool CurrentlyWorkingHtml = false;
+        public enum TorrentWebsite
+        {
+            ThePirateBay,
+            Kickass,
+        }
+
+        public enum Category
+        {
+            All,
+            Music,
+            MoviesVideos,
+            Applications,
+            Games,
+            Porn,
+            Others,
+        }
+
+        public static string Subtitle_Path = "";
+
+        public static bool IsValidUrl(string url)
+        {
+            Uri newUrl = null;
+            return Uri.TryCreate(url, UriKind.Absolute, out newUrl) && newUrl.Scheme == Uri.UriSchemeHttp;
+        }
+
+        public static string ParseUrl(string url)
+        {
+            if (url.StartsWith("http://") || url.StartsWith("https://"))
+                return url;
+
+            if (url.StartsWith("//"))
+                return "http://" + url.Substring(2, url.Length - 2);
+
+            if (url != "null")
+                return "http://" + url;
+
+            return "null";
+        }
+
+        public static string GetHtml(string url, bool needGzipDecompression = false)
+        {
+            try
+            {
+                if (!IsValidUrl(url))
+                    return "null";
+
+
+                switch (needGzipDecompression)
+                {
+                    case false:
+                        using (WebClient wc = new WebClient())
+                        {
+                            wc.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                            return wc.DownloadString(url);
+                        }
+
+                    case true:
+                        HttpWebRequest wr = WebRequest.CreateHttp(url);
+                        var response = wr.GetResponse();
+
+                        using (var z = new GZipStream(response.GetResponseStream(), CompressionMode.Decompress))
+                        using (var reader = new StreamReader(z, Encoding.UTF8))
+                            return reader.ReadToEnd();
+                }
+
+                return "null";
+            }
+            catch
+            {
+                System.Windows.Forms.MessageBox.Show("OI");
+                return "null";
+            }
+        }
+
+        public static bool IsSomethingPresentOnGrid(System.Windows.Forms.DataGridView dgv, string rowToCompare, string whatCompare)
+        {
+            try
+            {
+                foreach (System.Windows.Forms.DataGridViewRow r in dgv.Rows)
+                    if (r.Cells[rowToCompare].Value.ToString() == whatCompare)
+                        return true;
+
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+        public static Image GetImageFromUrl(string url, string toSave = "")
+        {
+            try
+            {
+                WebClient wc = new System.Net.WebClient();
+                byte[] imageBytes = wc.DownloadData(url);
+
+
+                MemoryStream ms = new MemoryStream(imageBytes);
+                Image currImg = Image.FromStream(ms);
+
+                if (toSave.Trim() != "")
+                    currImg.Save(toSave);
+
+                return currImg;
+            }
+            catch (Exception ex) { return null;  }
+        }
+    }
+}
